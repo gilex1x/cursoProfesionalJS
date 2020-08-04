50885bc20659ffc313e5d87adf928e7880ce9bf3
GILEXX <gilbertosa099@outlook.com>
2020-08-03 20:19:06 -0500

    Boton de pause y play

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,22 @@
 const video = document.querySelector("video")
 const button = document.querySelector("button")
 
-button.onclick =()=> video.play(); 
+function MediaPalyer(config){//Le pasamos un objeto
+  this.media= config.el;//aca llamammos al video
+}
+/*Los metodos tambien se pueden crear fuera del prototype 
+con objeto.prototype.metodo = funcion(){}
+*/
+debugger;
+MediaPalyer.prototype.play = function(){//Creamos el metodo de la "clase"
+  if(this.media.paused){
+    this.media.play();
+  }else{
+    this.media.pause();
+  }
+}
+
+const player = new MediaPalyer({el:video});//enviamos el video
+
+button.onclick =()=>player.play();
+  
